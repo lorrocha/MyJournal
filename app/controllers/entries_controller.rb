97dd69becba10d5f1ea068ba12e834a52db9b4d2
Lorry Rocha <sorai120@yahoo.com>
2013-12-20 17:12:41 -0500

    End of the day commit. Added an Entry controller/ index view page

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,10 +1,17 @@
 class EntriesController < ActionController::Base
+  before_action :set_entry, only: [:show, :edit, :update, :destroy]
 
   def create
+    @entry = Entry.new(entry_params)
+
+    if @entry.save
+      redirect_to @entry, notice: 'Entry was successfully created!'
+    else
+      render action:'new'
+    end
   end
 
   def show
-    @entry = Entry.find(params[:id])
   end
 
   def index
@@ -15,11 +22,28 @@ class EntriesController < ActionController::Base
   end
 
   def new
+    @entry = Entry.new
   end
 
   def update
+    if @entry.update(entry_params)
+      redirect_to @entry, notice: 'Entry was successfully updated!'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    @entry.destroy
+  end
+
+  private
+
+  def set_entry
+    @entry = Entry.find(params[:id])
+  end
+
+  def entry_params
+    params.require(:entry).permit(:title, :description, :category_id)
   end
 end
